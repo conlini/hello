44038912fe2ac10eb65f63833e0056fad7e17f7e
Aditya Bhasin <conlini@gmail.com>
2017-11-02 20:39:59 +0530

    added basic ui

---
+++ hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Aditya Bhasin on 02/11/17.
+//  Copyright © 2017 Conlini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
